287d2bef335028f82f2abc9b4b11f9e927f9e7f9
KDG3536 <81103357+Kim-DG@users.noreply.github.com>
2022-01-24 02:37:14 +0900

    컬러, 폰트 추가

--- app/src/main/java/com/checkmooney/naeats/ui/theme/Type.kt
+++ app/src/main/java/com/checkmooney/naeats/ui/theme/Type.kt
@@ -2,14 +2,21 @@ package com.checkmooney.naeats.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.checkmooney.naeats.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(
+            Font(
+                R.font.cafe24surround_air,
+                FontWeight.Normal
+            )
+        ),
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
