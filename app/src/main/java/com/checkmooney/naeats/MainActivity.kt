6e143d81c3b5ae04f8363173decb8b634d299d12
KDG3536 <81103357+Kim-DG@users.noreply.github.com>
2022-01-29 14:20:05 +0900

    상단 메뉴바 생성

--- app/src/main/java/com/checkmooney/naeats/MainActivity.kt
+++ app/src/main/java/com/checkmooney/naeats/MainActivity.kt
@@ -5,18 +5,27 @@ package com.checkmooney.naeats
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.Typography
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.checkmooney.naeats.databinding.ContentMainBinding
-import com.checkmooney.naeats.ui.theme.mainText
+import com.checkmooney.naeats.ui.theme.LineLightGrey
+import com.checkmooney.naeats.ui.theme.titleModifier
+import com.checkmooney.naeats.ui.theme.titleText
 import com.google.android.material.navigation.NavigationView
 import java.lang.reflect.Type
 import java.time.format.TextStyle
@@ -43,11 +52,34 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun TodayRecommend() {
     Scaffold() {
-        Column() {
+        Column {
             Text(
                 text = "오늘의 추천 메뉴",
-                style = mainText,
+                style = titleText,
+                modifier = titleModifier,
             )
+            RecommendIcon()
+            UnderBar()
         }
     }
+}
+
+@Composable
+fun RecommendIcon() {
+    Row(horizontalArrangement = Arrangement.SpaceAround, modifier = Modifier
+        .padding(start = 8.dp, end = 8.dp, bottom = 16.dp)
+        .fillMaxWidth()){
+        Icon(Icons.Rounded.Menu, contentDescription = "Localized description", modifier = Modifier.size(30.dp))
+        Icon(Icons.Rounded.Menu, contentDescription = "Localized description", modifier = Modifier.size(30.dp))
+        Icon(Icons.Rounded.Menu, contentDescription = "Localized description", modifier = Modifier.size(30.dp))
+    }
+}
+
+@Composable
+fun UnderBar(){
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .height(2.dp)
+            .background(color = LineLightGrey))
 }
--- app/src/main/java/com/checkmooney/naeats/ui/theme/Type.kt
+++ app/src/main/java/com/checkmooney/naeats/ui/theme/Type.kt
@@ -1,12 +1,15 @@
 package com.checkmooney.naeats.ui.theme
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.checkmooney.naeats.R
+import androidx.compose.ui.Modifier
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -34,7 +37,7 @@ val Typography = Typography(
     */
 )
 
-val mainText = TextStyle(
+val titleText = TextStyle(
     fontFamily = FontFamily(
         Font(
             R.font.cafe24surround_air,
@@ -44,3 +47,5 @@ val mainText = TextStyle(
     fontWeight = FontWeight.Normal,
     fontSize = 25.sp
 )
+
+val titleModifier = Modifier.padding(start = 24.dp, top = 64.dp, bottom =64.dp)
