a516937937d537ee0bbf36945873f8a4ef790536
SeungYeon Kim <k_sy_3@naver.com>
2022-01-30 21:05:35 +0900

    MainFragment scaffold 작업

--- app/src/main/java/com/checkmooney/naeats/MainActivity.kt
+++ app/src/main/java/com/checkmooney/naeats/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.checkmooney.naeats
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.navigation.NavGraph
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.checkmooney.naeats.components.BottomNavigationBar
+import com.checkmooney.naeats.components.NaEatsScaffold
 import com.checkmooney.naeats.databinding.ContentMainBinding
+import com.checkmooney.naeats.ui.theme.NaEatsTheme
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -15,10 +27,5 @@ class MainActivity : AppCompatActivity() {
 
         viewBinding = ContentMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
-        val navView: NavigationView = viewBinding.navView
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        navView.setupWithNavController(navController)
-        
     }
 }
