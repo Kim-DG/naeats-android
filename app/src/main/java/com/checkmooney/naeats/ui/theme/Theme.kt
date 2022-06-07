e99e1b7debeccc2b7a9f0224f2b8ca5ce709f2b8
KDG3536 <81103357+Kim-DG@users.noreply.github.com>
2022-06-07 13:23:48 +0900

    다크모드 수정

--- app/src/main/java/com/checkmooney/naeats/ui/theme/Theme.kt
+++ app/src/main/java/com/checkmooney/naeats/ui/theme/Theme.kt
@@ -29,7 +29,7 @@ private val LightColorPalette = lightColors(
 @Composable
 fun NaEatsTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        LightColorPalette
     } else {
         LightColorPalette
     }
